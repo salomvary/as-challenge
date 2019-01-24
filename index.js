fb584ff73a0791ef4c3597ea903c8fbec9fecf10
Márton Salomváry <salomvary@gmail.com>
2019-01-24 17:00:58 +0100

    Add some comments

--- index.js
+++ index.js
@@ -9,31 +9,51 @@ const app = new Vue({
   },
 
   methods: {
+    /**
+     * Fetch a user and show a loading message
+     * or an error message on failure
+     */
     async fetchUser () {
       this.message = 'Fetching user...'
       try {
         this.user = await fetchUser(1)
         this.message = null
       } catch (e) {
-        console.error('Failed to fetch user', e)
         this.message = 'Failed to fetch user :('
+        console.error('Failed to fetch user', e)
       }
     }
   },
 
+  /**
+   * @see https://vuejs.org/v2/api/#created
+   */
   async created () {
     this.fetchUser()
   }
 })
 
+/**
+ * Fetch user by id from the API
+ *
+ * @param {string|number} userId
+ * @returns {object} the user object
+ */
 async function fetchUser (userId) {
   const [profile, posts] = await Promise.all([
     fetchJSON(`${baseUrl}/users/${encodeURIComponent(userId)}`),
     fetchJSON(`${baseUrl}/posts?userId=${encodeURIComponent(userId)}`)
   ])
+  // Merge the user profile data and posts
   return { profile, posts }
 }
 
+/**
+ * Fetch URL and parse a successful response as JSON
+ *
+ * @param {string} url
+ * @returns {object} the parsed response
+ */
 async function fetchJSON (url) {
   const response = await fetch(url)
   if (response.ok) {
